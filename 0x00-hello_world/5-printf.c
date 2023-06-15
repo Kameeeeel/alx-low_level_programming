93bc5e7b5983b508e83cccc37950a8e2a81c2cf8
kameeeeel <abanoubkameel.nashed@gmail.com>
2023-06-15 09:14:39 -0700

    this is my general commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main function to print the sentence with proper grammar, but the outcome is a piece of art,
+ *
+ * Description: A C program that uses a printf function
+ * 
+ * return: must be zero
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
