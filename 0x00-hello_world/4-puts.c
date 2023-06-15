77e3700d52c0c5fa3c6cbcffdb205febb991cc48
kameeeeel <abanoubkameel.nashed@gmail.com>
2023-06-15 08:50:16 -0700

    this is my general commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(void){
-	puts("Programming is like building a multilingual puzzle")\n;
-	return 0;
+
+/*
+ * main = entry point
+ *
+ * Description: A C program with puts function
+ *
+ * return: Zero is the target
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
