b029d53f7d483970f46abc343e104817c89bd6e6
kameeeeel <abanoubkameel.nashed@gmail.com>
2023-06-16 07:11:14 -0700

    this is my general commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /* more headers goes there */
 /**
  * main - entry point
  * Description: this a C prgram that uses If statement
  * Return: always 0 (Success)
-*/
+ */
 /* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
