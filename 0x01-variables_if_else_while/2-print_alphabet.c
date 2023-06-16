c884a45de11dc96b28e718cb9fb46bee53746a6d
kameeeeel <abanoubkameel.nashed@gmail.com>
2023-06-16 09:03:12 -0700

    this is my general commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
+
 /**
  * main - entry point
  *
  * Description: A C program that uses putchar function
  *
- * Return always 0 (Success)
+ * Return: always 0 (Success)
 */
 
+
 int main(void)
+
 {
 	putchar l = 'a';
+
 	while (l <= 'z')
 	{
 		printf("%d", l);
