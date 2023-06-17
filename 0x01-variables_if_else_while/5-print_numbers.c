7685bc53350dc27e7982f3844604edfb60c119ba
kameeeeel <abanoubkameel.nashed@gmail.com>
2023-06-17 07:13:30 -0700

    this is my general commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program that uses printf function
+ *
+ * Return: must be 0 (Success)
+*/
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		printf("%d", d);
+		d++;
+	}
+	return (0);
+}
