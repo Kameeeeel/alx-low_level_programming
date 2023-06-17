c5d65d362218034272627bef3eee86fd9925c6ba
kameeeeel <abanoubkameel.nashed@gmail.com>
2023-06-17 06:48:14 -0700

    this is my general commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program that uses both for loop and putchar
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	char = ch
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (cha = 'A'; cha <= 'Z'; cha++)
+	{
+		putchar(cha);
+	}
+	putchar('\n');
+	return (0);
+}
