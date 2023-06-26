5d2a8c858ead691e971f6763bf933771cdea5ce8
kameeeeel <abanoubkameel.nashed@gmail.com>
2023-06-26 07:29:52 -0700

    this is my general commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,4 +8,16 @@
  * Return: Must be always 0 (Success) 
 */
 
-
+void main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
