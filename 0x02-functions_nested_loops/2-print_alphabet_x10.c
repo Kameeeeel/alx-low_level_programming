fc5a44f8486ff03281eceedbc4585175790b02e8
kameeeeel <abanoubkameel.nashed@gmail.com>
2023-06-26 08:53:31 -0700

    this is my general commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * print_alphabet_x10 - A C program that uses _putchar function
+ *			to print alphabet ten times
+*/
+
+void print_alphabet_x10(void)
+{
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+}
