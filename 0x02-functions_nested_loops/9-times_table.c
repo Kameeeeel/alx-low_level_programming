87a498d744aeb3869870438832d089f61664170a
kameeeeel <abanoubkameel.nashed@gmail.com>
2023-06-27 03:44:23 -0700

    this is my general commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,35 +2,32 @@
 
 /**
  * times_table - prints the 9 times table, starting with 0
- */
+*/
+
 void times_table(void)
 {
-	int i, j, k;
-
-	for (i = 0; i < 10; i++)
+	for (c = 0; c < 10; c++)
 	{
-		for (j = 0; j < 10; j++)
+		for (n=0; n< 10; n++)
 		{
-			k = j * i;
-			if (j == 0)
+			t = c * n
+			if (n = 0)
 			{
-				_putchar(k + '0');
+				_putchar(t);
 			}
-
-			if (k < 10 && j != 0)
+			else if (t < 10 && n != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10)
+				_putchar(t);
+			}
+			else
 			{
-				_putchar(',');
 				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				_putchar(t);
+				_putchar(',');
 			}
 		}
-		_putchar('\n');
 	}
 }
