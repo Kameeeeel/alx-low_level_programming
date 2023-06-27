87fcbde1eb68feee39c7f1c4efc4098d6b0f2e0d
kameeeeel <abanoubkameel.nashed@gmail.com>
2023-06-27 13:49:36 -0700

    this is my general commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,24 +7,15 @@
  */
 int main(void)
 {
-	unsigned long int sum3, sum5, sum;
-	int i;
+	int n, sum;
 
-	sum3 = 0;
-	sum5 = 0;
-	sum = 0;
-
-	for (i = 0; i < 1024; ++i)
+	for (n = 0; n < 1024; n++)
 	{
-		if ((i % 3) == 0)
+		if ((n % 3 == 0) || (n % 5 == 0))
 		{
-			sum3 = sum3 + i;
-		} else if ((i % 5) == 0)
-		{
-			sum5 = sum5 + i;
+			sum += n;
 		}
 	}
-	sum = sum3 + sum5;
-	printf("%lu\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
