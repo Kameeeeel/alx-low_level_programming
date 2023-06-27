84184c2821c54e30909f01d01ef9c723a01d83df
kameeeeel <abanoubkameel.nashed@gmail.com>
2023-06-27 13:38:03 -0700

    this is my general commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,9 @@ void print_times_table(int n)
 	int i, j, k;
 
 	if (n >= 0 && n <= 15)
+	{
 		for (i = 0; i <= n; i++)
+		{
 			for (j = 0; j <= n; j++)
 			{
 				k = i * j;
@@ -22,16 +24,14 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 100)
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
+				} else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -41,4 +41,6 @@ void print_times_table(int n)
 				}
 			}
 			_putchar('\n');
+		}
+	}
 }
