7a54ab3b89fda6b47c4298b883ed8cb2bf48d61b
kameeeeel <abanoubkameel.nashed@gmail.com>
2023-06-28 11:26:21 -0700

    s

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,12 @@ void more_numbers(void)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			_putchar(n + '0');
+			if (n >= 10)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
 		}
-	}
-	_putchar('\n');
+		_putchar('\n');
+	}	
 }
