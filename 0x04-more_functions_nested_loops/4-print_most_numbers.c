515d57c0497d6c426a3494873156995d5de26861
kameeeeel <abanoubkameel.nashed@gmail.com>
2023-06-28 11:11:03 -0700

    s

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,11 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = '0'; n < '10'; n++)
 	{
 		if (n != 2 || n != 4)
 		{
-			_putchar(n + 48);
+			_putchar(n + '0');
 		}
 	}
 	_putchar('\n');
